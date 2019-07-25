fdfa584383c08c0a4ad3a204d4009f8af300f705
Leath <leathatecrew@gmail.com>
2019-07-25 21:13:59 +0200

    We are able to read a file. That's awesome.

---
+++ README.md
@@ -0,0 +1,10 @@
+## Ranaz
+Really, AN Attempt Zero
+
+### Run it
+cd rana
+echo 'Hello World' > tmpFile
+cargo run tmpFile 
+rm tmpFile
+
+It prints file content because it's *awesome*.
---
+++ rana/src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn analyze(filename : &str) -> std::io::Result<()> { 
+    let mut file = File::open(filename)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!("{}",contents);
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        // no arguments passed
+        2 => {
+            let filename = &args[1];
+            analyze(filename);
+        },
+        _ => {
+            println!("This is an unstoppable killing machine");
+        }
+    };
+    Ok(())
+}
