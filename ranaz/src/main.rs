93eba75f0a52af88b90c22aa71c382aba22fed4e
Leath <leathatecrew@gmail.com>
2019-07-27 23:48:20 +0200

    Dummy commit

--- ranaz/src/main.rs
+++ ranaz/src/main.rs
@@ -2,10 +2,21 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
+fn countOnesInByte(b: Byte) -> u8 {
+    let mut c = 0;
+    for x in 0..8 {
+        if b & 1 == 1 {
+            c += 1;
+        }
+    }
+    c
+}
+
 fn analyze(filename : &str) -> std::io::Result<()> { 
     let mut file = File::open(filename)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
+    for byte in contents.bytes();
     println!("{}",contents);
     Ok(())
 }
