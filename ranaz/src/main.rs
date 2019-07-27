22859fe2754486c6e9b058ea44bbea730dd22e08
neronatac <neronatac@hotmail.fr>
2019-07-27 19:42:11 +0200

    Added some Markov computations

--- ranaz/src/main.rs
+++ ranaz/src/main.rs
@@ -1,12 +1,14 @@
-
 #[macro_use]
 extern crate clap;
+
+pub mod markov;
+pub mod fourier;
 
 use clap::App;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn analyze(filename : &str) -> std::io::Result<()> { 
+fn analyze(filename : &str) -> std::io::Result<()> { // TODO: pass the File object as parameter?
     let mut file = File::open(filename)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
@@ -14,16 +16,34 @@ fn analyze(filename : &str) -> std::io::Result<()> {
     Ok(())
 }
 
+// TODO: use a logger system and print more if verbose_level is high?
+
 fn main() -> std::io::Result<()> {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
+
+    let mut input_file = File::open(matches.value_of("INPUT").unwrap())?;
 
     let verbose_level = match matches.occurrences_of("v") {
         lvl if lvl < 3 => lvl,
         _ => panic!("Cannot take more than 2 verbose arguments.")
     };
 
-    analyze(matches.value_of("INPUT").unwrap());
+    if let Some(matches) = matches.subcommand_matches("markov") {
+        if matches.is_present("png") {
+            let mut bytes = Vec::<u8>::new();
+            input_file.read_to_end(&mut bytes)?;
+            let matrix = markov::get_markov_array(&bytes);
+            markov::to_img(&matrix,
+                matches.value_of("OUTPUT").unwrap());
+        }
+        else {
+            println!("No command => does nothing...")
+        }
+    }
+    else {
+        analyze(matches.value_of("INPUT").unwrap());
+    }
 
     Ok(())
 }
---
+++ ranaz/src/markov.rs
@@ -0,0 +1,70 @@
+//! Implements some Markov computations
+
+extern crate image;
+
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+
+/// Returns the Markov matrix (1rst values are the lines, 2nd ones are the columns) of a given
+/// sequence of bytes.
+///
+/// # Arguments
+/// * 'bytes' - a vector of u8
+///
+/// # Examples
+/// ```no_run
+/// let mut input_file = File::open("my_bin");
+/// let mut bytes = Vec::<u8>::new();
+/// input_file.read_to_end(&mut bytes)?;
+/// let matrix = get_markov_array(&bytes);
+/// ```
+pub fn get_markov_array(bytes: &Vec<u8>) -> [[u32; 256]; 256] {
+    let mut matrix : [[u32; 256]; 256] = [[0; 256]; 256];
+    let mut old: u8 = 0;
+
+    for b in bytes.iter() {
+        matrix[old as usize][*b as usize] += 1;
+        old = *b;
+    }
+
+    matrix
+}
+
+/// Exports the given Markov matrix to a PNG file
+///
+/// # Arguments
+/// * matrix - reference to the matrix
+/// * filename - path to the file to create/write
+///
+/// # Examples
+/// ```no_run
+/// let matrix = get_markov_array(&bytes);
+/// to_img(&matrix, "my_img.png");
+/// ```
+pub fn to_img(matrix: &[[u32; 256]; 256], filename: &str) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+
+    // get the highest value in the matrix
+    let max = matrix
+        .iter()
+        .flat_map(|line| line.iter())
+        .max()
+        .unwrap();
+
+    println!("Max= {}", max);
+
+    // Normalise u8 values + flatten the matrix
+    let mut pixels : [u8; 256*256] = [0; 256*256];
+    for i in 0 .. 256 {
+        for j in 0 .. 256 {
+            pixels[i * 256 + j] = (matrix[i][j] * 256 / *max) as u8;
+        }
+    }
+
+    // write the PNG file
+    encoder.encode(&pixels, 256, 256, ColorType::Gray(8))?;
+
+    Ok(())
+}
