db1a7258016d245e0d0a29d4526c3ea53ea46287
Leath <leathatecrew@gmail.com>
2019-07-28 11:20:19 +0200

    Mono bit test implem and error handling fixed

--- ranaz/src/main.rs
+++ ranaz/src/main.rs
@@ -6,22 +6,36 @@ use clap::App;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn countOnesInByte(b: Byte) -> u8 {
+/* mono_bit_stat 
+ *  Return a tuple (zeroes, ones) which are respectively the number of ones
+ *  and zeros, as single bits, in the file content.
+ *  INPUT : String
+ *  OUTPUT: (nb_zeroes, nb_ones)
+ */
+fn mono_bit_stat(s: &mut String) -> (u128, u128) {
     let mut c = 0;
-    for x in 0..8 {
-        if b & 1 == 1 {
-            c += 1;
+    let tot = (s.chars().count() * 8) as u128;
+    for bi in s.as_bytes() {
+        let mut b = *bi;
+        for _ in 0..8 {
+            if b & 1 == 1 { c += 1; }
+            b >>= 1;
         }
     }
-    c
+    ((tot-c) as u128, c)
 }
 
 fn analyze(filename : &str) -> std::io::Result<()> { 
     let mut file = File::open(filename)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    for byte in contents.bytes();
-    println!("{}",contents);
+    /* Bit test */
+    let (count_z, count_o) = mono_bit_stat(&mut contents);
+    println!("Bit test : Z({}), O({})", count_z, count_o);
+    let mut bit_diff = count_z - count_o;
+    bit_diff *= bit_diff;
+    bit_diff /= count_z + count_o; 
+    println!("Bit test === {}", bit_diff);
     Ok(())
 }
 
@@ -29,12 +43,12 @@ fn main() -> std::io::Result<()> {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let verbose_level = match matches.occurrences_of("v") {
+    let _verbose_level = match matches.occurrences_of("v") {
         lvl if lvl < 3 => lvl,
         _ => panic!("Cannot take more than 2 verbose arguments.")
     };
 
-    analyze(matches.value_of("INPUT").unwrap());
+    analyze(matches.value_of("INPUT").unwrap())?;
 
     Ok(())
 }
